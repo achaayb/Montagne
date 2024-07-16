fce977fc4f441c9c5f3ba0246e35af5d8e654609
achaayb <achaaybdev@gmail.com>
2024-07-16 20:36:57 +0100

    Revamp project

--- client.py
+++ client.py
@@ -1,71 +1,67 @@
+import json
 import logging
 import socket
 import struct
+from typing import Any
 
-from constants import (B_AUTHENTICATE, B_MESSAGE, STR_AUTH_FAILURE,
-                       STR_AUTH_SUCCESS)
+from constants import (B_CONNECT, B_DISCONNECT, B_EVENT_TRIGGER, B_EVENT_RESPONSE,
+                       B_CONNECTED, B_DISCONNECTED, B_REFUSED)
 
 logging.basicConfig(level=logging.INFO)
 
 
-class Client:
-    def __init__(self, host, port, username="username", password="password"):
+class MontagneClient:
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.username = username
-        self.password = password
         self.socket = None
 
-    def connect(self):
-        try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-            packet = self._create_packet(
-                B_AUTHENTICATE, f"{self.username}:{self.password}"
-            )
-            self.socket.sendall(packet)
-            response = self.socket.recv(24)
-            if response == STR_AUTH_SUCCESS.encode("utf-8"):
-                logging.info("Authentication success")
-            elif response == STR_AUTH_FAILURE.encode("utf-8"):
-                logging.info("Authentication failure")
+    def connect(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
 
-        except ConnectionRefusedError:
-            logging.info("Connection refused")
-        except Exception as e:
-            logging.info(f"Error connecting to server: {e}")
+    def send_event(self, tag: str, args: list[Any] = None, kwargs: dict[str: Any] = None) -> Any:
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
+        payload_dict = {
+            "tag": tag,
+            "args": args,
+            "kwargs": kwargs
+        }
+        payload_json = json.dumps(payload_dict)
+        intro_payload = self._create_payload(B_EVENT_TRIGGER, payload_json)
+        self.socket.sendall(intro_payload)
+        
 
-    def _create_packet(self, packet_type, payload):
-        payload_bytes = payload.encode("utf-8")
-        payload_len = len(payload_bytes)
-        # Is not actually a header but
-        # the server will read first bytes then decide
-        # if he wants to keep reading.. to avoid spam x)
-        header = struct.pack("!BI", packet_type, payload_len)
-        packet = header + payload_bytes
-        return packet
-
-    def send(self, payload):
-        packet = self._create_packet(B_MESSAGE, payload)
-        self.socket.sendall(packet)
-        response = self.socket.recv(4096).decode("utf-8")
-        return response
-
-    def disconnect(self):
+    def disconnect(self) -> bool:
         if self.socket:
             self.socket.close()
-            logging.info("Disconnected from server")
+            return True
+        return False
 
+    def _create_payload(self, packet_type, body) -> bytes:
+        body_bytes = body.encode("utf-8")
+        body_len = len(body_bytes)
+        header = struct.pack("!HI", packet_type, body_len)
+        payload = header + body_bytes
+        return payload
 
 if __name__ == "__main__":
-    client = Client("localhost", 5000)
+    client = MontagneClient("localhost", 5000)
     client.connect()
+
+    client.send_event("kaka", [1,2,3], {"sda": "asdas"})
 
     try:
         while True:
-            message = input("message: ")
-            response = client.send(message)
-            logging.info(response)
+            foo = int(input("foo: "))
+            if foo == 1:
+                client.send_event("kaka", [1,2,3], {"sda": "asdas"})
+            elif foo == 2:
+                client.send_event("kdasdsaka", [1,2,3], {"sda": "asdas"})
+            #logging.info(response)
     except KeyboardInterrupt:
         logging.info("Manual exit")
     finally:
--- server.py
+++ server.py
@@ -1,92 +1,88 @@
+import json
 import logging
 import socket
 import struct
 from threading import Thread
 
-from constants import (B_AUTHENTICATE, LIST_DEFAULT_CREDENTIALS,
-                       STR_AUTH_FAILURE, STR_AUTH_SUCCESS)
+from constants import (B_ACCEPTED, B_CONNECT, B_DISCONNECT, B_EVENT_DOESNT_EXIST, B_EVENT_TRIGGER,
+                       B_CONNECTED, B_DISCONNECTED, B_REFUSED)
 
 logging.basicConfig(level=logging.INFO)
 
 
-class AuthManager:
-    def __init__(self, credentials: list[tuple[str, str]] | None = None):
-        self.credentials = (
-            credentials if credentials else LIST_DEFAULT_CREDENTIALS
-        )  # noqa
-
-    def check_credentials(self, username, password):
-        if (username, password) in self.credentials:
-            return True
-        return False
-
-
-class Server:
+class MontagneServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.server = None
         self.connections = []
-        self.auth_manager = AuthManager()
+        self.events = {}
 
-    def start(self):
+    def run(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, self.port))
         self.server.listen(5)
-        logging.info(f"SQLite server listening on {self.host}:{self.port}")
+        logging.info(f"Montagne server listening on {self.host}:{self.port}")
 
         while True:
             client_socket, client_address = self.server.accept()
-            logging.info(f"Accepted connection from {client_address}")
-            # First 5 bytes are always packet type.
-            # this protects against spam...
-            introduction_packet = client_socket.recv(5)
-            # B unsigned char
-            # I unsigned int
-            packet_type, next_len = struct.unpack("!BI", introduction_packet)
-            if packet_type == B_AUTHENTICATE:
-                logging.info(f"Auth packet received from {client_address}")
-                credentials_packet = client_socket.recv(next_len)
-                credentials_packet = credentials_packet.decode("utf-8")
-                username, password = credentials_packet.split(":")
-                if self.auth_manager.check_credentials(username, password):
-                    logging.info(f"Auth accepted from {client_address}")
-                    client_socket.sendall(STR_AUTH_SUCCESS.encode("utf-8"))
-                else:
-                    logging.info(f"Auth denied from {client_address}")
-                    client_socket.sendall(STR_AUTH_FAILURE.encode("utf-8"))
-                    continue
+            logging.info(f"Client {client_address} connected")
             # Creates a thread per connection.
             client_handler = Thread(
-                target=self._handle_connection,
+                target=self._connection_handler,
                 args=(client_socket, client_address),  # noqa
             )
             client_handler.start()
             self.connections.append(client_socket)
 
+    def event(self, tag=None):
+        def decorator(func):
+            self.events[tag] = func
+            return func
+        return decorator
+
+    def _connection_handler(self, client_socket, client_address):
+        while True:
+            intro_payload = client_socket.recv(6)
+            p_type, b_len = struct.unpack("!HI", intro_payload)
+            if p_type == B_EVENT_TRIGGER:
+                payload_body = client_socket.recv(b_len)
+                payload_json = json.loads(payload_body)
+                event_tag = payload_json.get("tag", None)
+                event_args = payload_json.get("args", [])
+                event_kwargs = payload_json.get("kwargs", {})
+                event = self.events.get(event_tag, None)
+                if not event:
+                    event_not_found_payload = self._create_payload(B_EVENT_DOESNT_EXIST, "")
+                    client_socket.sendall(event_not_found_payload)
+                    logging.info(f"Event {event_tag} not found")
+                else:
+                    event(client_socket, *event_args, **event_kwargs)
+                    print(f"Event {event_tag} done")
+            else:
+                continue
+
+
+    def _create_payload(self, packet_type, body):
+        body_bytes = body.encode("utf-8")
+        body_len = len(body_bytes)
+        header = struct.pack("!BI", packet_type, body_len)
+        payload = header + body_bytes
+        return payload
+    
     def close(self):
         self.server.close()
 
-    def _close_connection(self, client_socket):
-        client_socket.close()
-
-    def _handle_connection(self, client_socket, client_address):
-        try:
-            while True:
-                request = client_socket.recv(4096).decode("utf-8")
-                # If connection is closed .recv returns b''
-                if not request:
-                    break
-
-                client_socket.sendall(request.encode("utf-8"))
-        except Exception as e:
-            print(f"Error handling client: {e}")
-        finally:
-            self._close_connection(client_socket)
-            logging.info(f"Connection Disconnected {client_address}")
 
 
 if __name__ == "__main__":
-    server = Server("localhost", 5000)
-    server.start()
-    server.close()
+    montagne_app = MontagneServer("localhost", 5000)
+
+    @montagne_app.event("kaka")
+    def foo(socket, *args, **kwargs):
+        socket.sendall("hi".encode("utf-8"))
+
+    try:
+        montagne_app.run()
+    except KeyboardInterrupt:
+        montagne_app.close()
